31483a6acba41f27cffc37404571c3727e171998
vN011 <viniiexl077@gmail.com>
2026-08-21 16:46:51 -0300

    Add files via upload

---
+++ 6 - antecessor_sucessor.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+# Solicitando Dados
+print("= Solicitando Dados =")
+numero = int(input("Digite um Número: "))
+antecessor = numero + 1
+sucessor =  numero - 1
+
+print("\n= Exibindo Dados =")
+print("numero: ", numero)
+print("antecessor: ", antecessor)
+print("sucessor: ", sucessor)
+
